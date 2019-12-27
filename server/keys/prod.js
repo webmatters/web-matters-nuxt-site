fb8b56a4de9b5bec43761c8feeeba9357237e9af
Andrew Smith <andrew.smith@webmatters.io>
2019-12-26 21:12:20 -0500

    Fixes production variables

--- server/keys/prod.js
+++ server/keys/prod.js
@@ -2,4 +2,3 @@ module.exports = {
   DB_URI: process.env.DB_URI,
   SESSION_SECRET: process.env.SESSION_SECRET
 }
-;('mongodb+srv://wmAdmin:l=pvQXzh26#8rIUg@cluster0-scior.mongodb.net/test?retryWrites=true&w=majority')
