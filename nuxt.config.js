24c5a3a6e8eaa0989ac93fac3be5c1d065cdfac1
Andrew Smith <andrew.smith@webmatters.io>
2019-12-26 20:57:51 -0500

    Makes changes for heroku deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,9 @@ module.exports = {
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000'
+  },
   serverMiddleware: ['~/server/routes/index'],
   /*
    ** Build configuration
--- server/keys/prod.js
+++ server/keys/prod.js
@@ -2,3 +2,4 @@ module.exports = {
   DB_URI: process.env.DB_URI,
   SESSION_SECRET: process.env.SESSION_SECRET
 }
+;('mongodb+srv://wmAdmin:l=pvQXzh26#8rIUg@cluster0-scior.mongodb.net/test?retryWrites=true&w=majority')
